dbe4708cc1030f6380ff0f4e5a5449f1b105059c
wlx <118099004+AAAwlx@users.noreply.github.com>
2022-11-28 20:06:54 +0800

    Add files via upload

---
+++ 2.12.4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void jolly()
+{
+    printf("For he's a jolly good fellow!\n");
+}
+void deny()
+{
+    printf("Which nobody can deny!");
+}
+int main()
+{
+    jolly();
+    jolly();
+    jolly();
+    deny();
+}
