553c07fc2a0f3f45bcdb05ce661c3e82f4524810
wlx <118099004+AAAwlx@users.noreply.github.com>
2023-05-14 15:17:26 +0800

    Add files via upload

--- 函数指针练习.cpp
+++ 函数指针练习.cpp
@@ -1,18 +1,31 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<functional>
 /*void fucn(int value)
 {
     std::cout<<"value"<<value<<std::endl;
 }*/
-void hhh(const std::vector<int>& values,void(*func)(int))
+void hhh(const std::vector<int>& values,/*int(*func)(int)*/const std::function<int(int)>&func)
 {
     std::cout<<"hello"<<std::endl;
-    for(int value:values)
-        func(value);
+    for(int value:values){
+        int b=func(value);   
+    }
+        
 }
 
 int main()
 {
     std::vector<int>values={1,2,3,4,5};
-    hhh(values,[](int a){std::cout<<"value"<<a<<std::endl;});
+    int a=1;
+    
+    auto lambda=[=](int value)mutable{
+        a++;
+        std::cout<<"value"<<value<<a<<std::endl;
+        return value;
+    };
+    hhh(values,lambda);
+    std::cout<<a<<std::endl;
+    
 }
