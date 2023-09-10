de86bf964e60045a8892be80d46e739dcbe92c2a
ylx <561639418@qq.com>
2023-09-10 16:07:17 +0800

    ooo

--- python/B/1.py
+++ python/B/1.py
@@ -1,38 +1,36 @@
 import math
 import pandas as pd
+from sympy import symbols, Eq, solve
 
+x, y = symbols('x y')
 # 给定参数
 alpha = math.radians(1.5)  # 坡度，弧度表示
 theta = math.radians(120)  # 多波束开角，弧度表示
-o = math.radians(30)
-theta2 = math.radians(60)
-D0 = 70  # 海域中心点处的海水深度，单位：米
-
-# 表1中的测线距中心点处的距离
-distances1 = [-800, -600, -400, -200]
-distances2 = [200, 400, 600, 800]
-# 计算覆盖宽度和重叠率
-results1 = []
-for r in distances1:
-    deep=2*(70+r*math.tan(alpha))#距离中心的深度
-    a = deep * math.sin(theta)*math.cos(alpha)
-    b = math.cos(theta)+math.cos(o)
-    w = a/b#覆盖宽度
-    overlap = 1-r*b/2*deep*math.sin(theta)*math.cos(alpha)*(math.cos(alpha)+1/math.tan(theta2)) # 重叠率
-    results1.append([r, deep, w ,overlap])
-results2 = []
-for r in distances2:
-    deep=2*(70-r*math.tan(alpha))#距离中心的深度
-    a = deep * math.sin(theta)*math.cos(alpha)
-    b = math.cos(theta)+math.cos(o)
-    w = a/b#覆盖宽度
-    overlap = 1-r*b/2*deep*math.sin(theta)*math.cos(alpha)*(math.cos(alpha)+1/math.tan(theta2)) # 重叠率
-    results2.append([r, deep, w ,overlap])
-# 创建DataFrame并保存到xcel文件
-df1 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m', '覆盖宽度/m', '与前一条测线的重叠率/%'])
-df2 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m', '覆盖宽度/m', '与前一条测线的重叠率/%'])
-df1.to_excel('result1.xlsx', index=False)
-
-# 打印结果
+distances = [-800, -600, -400, -200, 0, 200, 400, 600, 800]
+results1= []
+i=0
+for d in distances:
+    deep = 70-d*math.tan(alpha)
+    w = abs(2*abs(deep)*math.cos(alpha)*math.sin(theta)/(math.cos(theta)+math.cos(math.radians(3))))
+    line1 = Eq(math.tan(math.radians(30)) * x -y,0)
+    line2 = Eq(165.45 - math.tan(math.radians(30)) * x - y)
+    line3 = Eq(math.tan(alpha)*(d+800+x) - y,0)
+    solution1 = solve((line1, line3), (x, y))
+    solution2 = solve((line2, line3), (x, y))
+    solution3 = solve((line1, line2), (x, y))
+    x1,y1=solution1[x],solution1[y]
+    x2,y2=solution2[x],solution2[y]
+    y3 = solution3[y]
+    if y3 > y1 or y3 > y2:#判断n的正负
+        f=1
+    else :
+        f=-1
+    distance1 = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)#计算两交点之间的距离
+    if i==0:
+        n=0
+    else:
+        n=f*distance1/results1[i-1][3]#取出前一组测线覆盖宽度
+    i+=1
+    results1.append([d, deep, w, n ])
+df1 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m', '距离中心的深度/m', '覆盖宽度/m','y'])
 print(df1)
-print(df2)
--- python/B/2.py
+++ python/B/2.py
@@ -1,38 +1,31 @@
 import math
-import pandas as pd
+import pandas as pd#避免分母为零
 
 # 给定参数
 alpha = math.radians(1.5)  # 坡度，弧度表示
 theta = math.radians(120)  # 多波束开角，弧度表示
 o = math.radians(30)
 theta2 = math.radians(60)
-D0 = 70  # 海域中心点处的海水深度，单位：米
-
-# 表1中的测线距中心点处的距离
-distances1 = [-800, -600, -400, -200]
-distances2 = [200, 400, 600, 800]
+  # 海域中心点处的海水深度，单位：米
+gamma = [0,45,90,135,180,225,270,315]
+# 表2中的测线距中心点处的距离
+distances = [0,0.3,0.6,0.9,1.2,1.5,1.8,2.1,4]
 # 计算覆盖宽度和重叠率
-results1 = []
-for r in distances1:
-    deep=2*(70+r*math.tan(alpha))#距离中心的深度
-    a = deep * math.sin(theta)*math.cos(alpha)
-    b = math.cos(theta)+math.cos(o)
-    w = a/b#覆盖宽度
-    overlap = 1-r*b/2*deep*math.sin(theta)*math.cos(alpha)*(math.cos(alpha)+1/math.tan(theta2)) # 重叠率
-    results1.append([r, deep, w ,overlap])
-results2 = []
-for r in distances2:
-    deep=2*(70-r*math.tan(alpha))#距离中心的深度
-    a = deep * math.sin(theta)*math.cos(alpha)
-    b = math.cos(theta)+math.cos(o)
-    w = a/b#覆盖宽度
-    overlap = 1-r*b/2*deep*math.sin(theta)*math.cos(alpha)*(math.cos(alpha)+1/math.tan(theta2)) # 重叠率
-    results2.append([r, deep, w ,overlap])
+i=0
+for g in gamma:
+    results1 = []
+    print(g)
+    for r in distances:
+        beat=math.radians(90-g)
+        if g<=180:
+            deep=120-abs(r*1852*math.tan(alpha)*math.cos(beat))
+        else :
+            deep = 120+abs(r*1852*math.tan(alpha)*math.cos(beat))
+        w = abs(2*deep*math.sin(theta)*math.cos(alpha)/(math.cos(theta)+math.sin(math.radians(3))))
+        results1.append([r,w])
+    df1 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m','覆盖宽度/m'])
+    print(df1)
 # 创建DataFrame并保存到xcel文件
-df1 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m', '覆盖宽度/m', '与前一条测线的重叠率/%'])
-df2 = pd.DataFrame(results1, columns=['测线距中心点处的距离/m', '覆盖宽度/m', '与前一条测线的重叠率/%'])
-df1.to_excel('result1.xlsx', index=False)
 
+#df1.to_excel('result2.xlsx', index=False)
 # 打印结果
-print(df1)
-print(df2)
