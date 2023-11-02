b12aa7be44fe68c211c49c8056aaf7060df39ef7
AAAwlx <561639418@qq.com>
2023-11-02 08:43:00 +0800

    o

--- rust/1.rs
+++ rust/1.rs
@@ -1,10 +1,33 @@
-fn main() {
-    let x = plus_one(5);
 
-    println!("The value of x is: {}", x);
+fn main() {
+    let mut s1 = String::new();
+
+    // 创建可变引用
+    let s = &mut s1;
+
+    // 添加句子到 String 中
+    let sentence = "This is a sentence.";
+    s.push_str(sentence);
+
+    // 也可以使用 push 方法来添加字符
+    s.push(' '); // 添加一个空格
+    s.push_str("It continues.");
+
+    // 打印结果
+    println!("{}", s);
+    s.push_str("It continues.");
+    
+
+    println!("The length of '{}' .", s1);
+    
+        let mut s = String::from("hello");
+    
+        let r1 = &mut s;
+        println!("{}", r1);
+        let r2 = &mut s;
+        println!("{}", r2);
 }
 
-fn plus_one(x: i32) -> i32(y: i32) {
-    x + 1;
-    let y=0
-}   
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
